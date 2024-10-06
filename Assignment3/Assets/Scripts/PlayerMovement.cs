3caad254ac221a083b09d4acfe0d0e9ce21e2b72
Hoeun Lee <hoeunlee228@gmail.com>
2024-10-06 17:58:45 +0900

    [Feat] #7 - Implementing Shooting Game Basics

--- Assignment3/Assets/Scripts/PlayerMovement.cs
+++ Assignment3/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float rotationSpeed = 100f;
+    public float jumpForce = 5f;     // Force of the player's jump
+
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -30,6 +34,16 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) 
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
+        // Rotate the player based on mouse movement
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+
+        // Player jump using Spacebar
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
